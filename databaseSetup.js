82b806cc9fdfb0295200ee7bb451d5f9f21f61a8
Satoko <stogamori@gmail.com>
2014-12-01 17:19:26 -0800

    (demo) cut a demo branch which works on local mysql

--- databaseSetup.js
+++ databaseSetup.js
@@ -2,25 +2,25 @@ var mysql = require('mysql');
 
 var dbConnection;
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
+//   dbConnection = mysql.createConnection({
+//     host: "us-cdbr-azure-west-a.cloudapp.net",
+//     user: "bffa2351040f6c",
+//     password: "81c5b268",
+//     database: "thesisdb"
+//   });
+// } else {
   dbConnection = mysql.createConnection({
-    host: "us-cdbr-azure-west-a.cloudapp.net",
-    user: "bffa2351040f6c",
-    password: "81c5b268",
-    database: "thesisdb"
-  });
-} else {
-  dbConnection = mysql.createConnection({
-    host: "localhost",
-    user: "team",
-    password: "agate",
-    database: "yesno"
+    // host: "localhost",
+    // user: "team",
+    // password: "agate",
+    // database: "yesno"
 
-    // user: "root",
-    // password: "",
-    // database: "test"
+    user: "root",
+    password: "",
+    database: "test"
   });
-}
+// }
 
 dbConnection.connect();
 
