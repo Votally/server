b78b4364228788c6c1681263fa8306dba4151d3e
brybott <brybott@users.noreply.github.com>
2015-01-08 00:33:05 -0800

    (refactor) add getUsername to routes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.post('/user/logout', user.logout);
 
 //home route
 app.post('/home/checkUpdates', home.checkUpdates);
+app.post('/home/getUsername', home.getusername);
 
 // //friends route
 app.post('/friends/checkRequest', friends.checkRequest);
